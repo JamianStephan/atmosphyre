506ee43c602101508834007a12e866809b9c8983
Jay Stephan <Jay.Stephan@stfc.ac.uk>
2023-10-19 15:12:32 +0100

    test

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ setuptools.setup(
     version="0.0.3", 
     author="Jay Stephan", 
     author_email="Jay.Stephan@stfc.ac.uk", 
-    packages=["atmosphyre"], 
+    packages=["atmosphyre","numpy","astropy","math","scipy","matplotlib"], 
     description="A sample test package", 
     long_description=description, 
     long_description_content_type="text/markdown", 
     license='MIT', 
     install_requires=['numpy'] 
 ) 
+
+
